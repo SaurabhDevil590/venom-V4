4d872dcac13c7a3c00aea887eea0546c911793ff
SaurabhDevil59 <saurabhlungare100@gmail.com>
2024-07-22 12:15:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7339455572:AAGssfbubKk-CLKoGxjVvzrGXi19VZZ3ddg")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
